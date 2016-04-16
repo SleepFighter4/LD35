6ed5ab141aed6605dc57588cd8a1b9cd10c8137f
Pete <p-morley@hotmail.co.uk>
2016-04-16 19:27:54 +0100

    Client and server are now talking. A rectangle is drawn on the client side but it isn't currently being updated properly. I've also removed JQuery.

--- Player.js
+++ Player.js
@@ -1,8 +1,26 @@
 function Player(id) {
+	this.id = id;
+	this.x = 0.0;
+	this.y = 0.0;
+	this.vX = 0.0;
+	this.vY = 0.0;
+	this.angle = 0.0;
+	this.rotationSpeed = 10.0;
+	this.shiftFactor = 1.0;
+	this.keyState = {};
+	this.lastMovedTime = Date.now();
+
 	return {
-		id: id,
-		x: 0.0,
-		y: 0.0,
-		shiftFactor: 1.0
+		id: this.id,
+		x: this.x,
+		y: this.y,
+		vX: this.vX, // Velocity X px / sec
+		vY: this.vY, // Velocity Y px / sec
+		angle: this.angle, // deg
+		rotationSpeed: this.rotationSpeed, // deg / sec
+		shiftFactor: this.shiftFactor,
+		keyState: this.keyState,
+		lastMovedTime: this.lastMovedTime
 	};
 }
+exports.Player = Player;
--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,115 @@
 "use strict"
-var canvas = $("#game-canvas")[0];
+
+// Globals
+var player;
+var players = [];
+var objects = [];
+var scores = [];
+var canvas = document.getElementById('game-canvas');
 var ctx = canvas.getContext("2d");
+var keyState = {};
+var socket;
+
+//Utilities
+var usedKeys = [37, 38, 39, 40, 32];
+var KEY_CODES = {
+	LEFT: 37,
+	UP: 38,
+	RIGHT: 39,
+	DOWN: 40,
+	SPACE: 32
+}
+var TO_RADIANS = Math.PI/180; 
+
+// Event listeners
+window.addEventListener('keydown',function(e){
+	if (usedKeys.indexOf(e.which) != -1 && !keyState[e.which]) {
+		keyState[e.which] = true;
+		socket.emit('keyupdate', keyState);
+	}
+},true);
+window.addEventListener('keyup',function(e){
+	if (usedKeys.indexOf(e.which) != -1 && keyState[e.which]) {
+		keyState[e.which] = false;
+		socket.emit('keyupdate', keyState);
+	}
+},true);
+window.addEventListener("resize", function(){
+	updateCanvasSize();
+},true);
+window.addEventListener("blur", function(){
+	keyState = {}
+});
+
+// Start program
+registerSocketHooks();
+updateCanvasSize();	
+updateCanvas();
+
+function registerSocketHooks() {
+	socket = io();
+	socket.on('player', function(p) {
+		player = p;
+	})
+	socket.on('game data', function(obj) {
+		players = obj.players;
+		objects = obj.objects;
+		scores = obj.scores;
+	})
+	socket.on('ping', function(p){
+		socket.emit('ping response', p);
+	});
+}
+
+function updateCanvas() {
+	requestAnimationFrame(updateCanvas);
+	if (typeof(player) != 'undefined') {
+		//Clear canvas
+		//console.log(canvas.width)
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		drawObjects();
+		drawPlayers();
+		drawUI();
+	}
+}
+
+function updateCanvasSize() {
+	socket.emit('canvas size',{x:canvas.width,y:canvas.height});
+}
+
+function getLocalCoords(x, y) {
+	// Convert global coords to coords relative to the top left of the user's canvas.
+	return {
+		x:canvas.width/2 - (player.x-x),
+		y:canvas.height/2 - (player.y-y)
+	};
+}
+
+function drawObjects() {
+	for (var i = 0; i < objects.length; i++) {
+		// Draw
+	}
+}
+
+function drawPlayers() {
+	if (typeof(players) != 'undefined' && typeof(player) != 'undefined') {
+		for (var i = 0; i < players.length; i++) {
+			// Draw player
+			var coords = getLocalCoords(players[i].x, players[i].y);
+			ctx.beginPath();
+			ctx.rect(coords.x, coords.y, 50, 100);
+			ctx.stroke();
+			ctx.closePath();
+		}
+	}
+}
+
+function drawUI() {
+	if (typeof(player) != player.ping) {
+		//Display ping.
+		ctx.fillStyle="#fff";
+		ctx.font="12px Arial";
+		var pingText = "Ping: " + player.ping;
+		ctx.fillText(pingText, 38, 30);
+	}
+}
