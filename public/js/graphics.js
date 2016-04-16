c2e109fe651979d071482a580d2834589f733ad0
Philip Bruland <phil_7445@hotmail.co.uk>
2016-04-17 00:10:01 +0100

    Graphical updates working towards track rendering

--- public/js/graphics.js
+++ public/js/graphics.js
@@ -5,8 +5,11 @@ var CAR_BODY_COLOR="#FF00FF"
 var CAR_PANEL_COLOR="#FF0000"
 var CAR_GLASS_COLOR="#6699ff"
 
-function drawbuilding(color,pos_x,pos_y, width, height)
+var backgroundcanvas;
+
+function drawbuilding(ctx, color, pos_x, pos_y, width, height)
 {
+  console.log("OH HEY");
   //BODY
   ctx.fillStyle = color;
   ctx.fillRect(pos_x, pos_y, width, height);
@@ -35,16 +38,51 @@ function drawcar(x,y)
   //BOOT
   ctx.fillStyle = CAR_PANEL_COLOR;
   ctx.fillRect(x+0,x+85,50,20);
+  ctx.rotate(30*Math.PI/180);
 }
 
-function drawbackground(buildingarray, rotation)
+function initbackground()
 {
-  var canvas = document.getElementById('building-canvas');
-  var ctx = canvas.getContext("2d");
+  backgroundcanvas = document.createElement("canvas");
+}
 
+function drawbackground(buildingarray, rotation, canvas)
+{
+  var ctx = canvas.getContext("2d");*/
+  ctx.save();
+  ctx.translate(canvas.width/2, canvas.height/2);
+  ctx.rotate(rotation*Math.PI/180);
+  var i;
   for(i = 0; i < buildingarray.length; i++)
   {
-    drawbuilding();
+    drawbuilding(ctx,
+                 buildingarray[i].color,
+                 buildingarray[i].pos_x,
+                 buildingarray[i].pos_y,
+                 buildingarray[i].width,
+                 buildingarray[i].height);
+    console.log("color " + buildingarray.color);
   }
-  ctx.rotate(rotation);
+  ctx.restore();
+  console.log("rotation " + rotation);
+}
+
+function building(color, pos_x, pos_y, width, height)
+{
+  this.color = color;
+  this.pos_x = pos_x;
+  this.pos_y = pos_y;
+  this.width = width;
+  this.height = height;
+}
+
+/*var buildingarray = [
+  new building("#00FF00", 50, 50, 100, 100),
+  new building("#00FF00", 100, 100, 100, 100)
+]*/
+
+function draw(canvas)
+{
+  drawcar(12,12);
+  drawbackground(buildingarray, 5, canvas);
 }
