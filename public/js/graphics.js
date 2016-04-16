36b37131862d99a8dc1df7212c63a4999e9db66a
Philip Bruland <phil_7445@hotmail.co.uk>
2016-04-16 19:43:29 +0100

    Partial update to graphics, to prep for tabs to spaces.

--- public/js/graphics.js
+++ public/js/graphics.js
@@ -5,37 +5,46 @@ var CAR_BODY_COLOR="#FF00FF"
 var CAR_PANEL_COLOR="#FF0000"
 var CAR_GLASS_COLOR="#6699ff"
 
-function drawbuilding(color,x,y)
+function drawbuilding(color,pos_x,pos_y, width, height)
 {
-	var canvas = $("#building-canvas")[0];
-	var ctx = canvas.getContext("2d");
-
 	//BODY
 	ctx.fillStyle = color;
-	ctx.fillRect(0,0,x,y);
+	ctx.fillRect(pos_x, pos_y, width, height);
 }
 
-function drawcar()
+function drawcar(x,y)
 {
 	var canvas = $("#car-canvas")[0];
 	var ctx = canvas.getContext("2d");
 
 	//BODY
 	ctx.fillStyle = CAR_BODY_COLOR;
-	ctx.fillRect(0,0,50,100);
+	ctx.fillRect(x,y,50,100);
 	//BONNET
 	ctx.fillStyle = CAR_PANEL_COLOR;
-	ctx.fillRect(0,0,50,20);
+	ctx.fillRect(x,y,50,20);
 	//WINDSHIELD
 	ctx.fillStyle = CAR_GLASS_COLOR;
-	ctx.fillRect(5,25,40,15);
+	ctx.fillRect(x+5,y+25,40,15);
 	//ROOF
 	ctx.fillStyle = CAR_PANEL_COLOR;
-	ctx.fillRect(5,45,40,15);
+	ctx.fillRect(x+5,y+45,40,15);
 	//BACKWINDSHIELD
 	ctx.fillStyle = CAR_GLASS_COLOR;
-	ctx.fillRect(5,65,40,15);
+	ctx.fillRect(x+5,x+65,40,15);
 	//BOOT
 	ctx.fillStyle = CAR_PANEL_COLOR;
-	ctx.fillRect(0,85,50,20);
+	ctx.fillRect(x+0,x+85,50,20);
+}
+
+function drawbackground(buildingarray, rotation)
+{
+	var canvas = $("#building-canvas")[0];
+	var ctx = canvas.getContext("2d");
+
+	for(i = 0; i < buildingarray.length; i++)
+	{
+		drawbuilding(
+	}
+	ctx.rotate(rotation);
 }
