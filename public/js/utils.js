de06ab1cb40391d40bfdf5d027cba8d3655b6308
Eric Engestrom <eric@engestrom.ch>
2016-04-17 18:48:46 +0100

    pathToBoxes: add minimum box thickness

--- public/js/utils.js
+++ public/js/utils.js
@@ -5,17 +5,22 @@
  *         {w, h} = width & height
  */
 function pathToBoxes(path) {
+  // The box will be at least this large/long
+  var minThickness = 2;
+
   var boxes = [];
   var previousStep = path[path.length-1];
 
   for(var step = 0; step < path.length; step++) {
     var currentStep = path[step];
+    var width  = Math.abs(previousStep.x - currentStep.x);
+    var height = Math.abs(previousStep.y - currentStep.y);
 
     boxes.push({
       'x': (previousStep.x + currentStep.x) / 2,
       'y': (previousStep.y + currentStep.y) / 2
-      'w': Math.abs(previousStep.x - currentStep.x),
-      'h': Math.abs(previousStep.y - currentStep.y),
+      'w': Math.max(width,  minThickness),
+      'h': Math.max(height, minThickness),
     });
 
     previousStep = currentStep;
