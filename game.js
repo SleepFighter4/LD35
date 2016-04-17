10595e3b4c20ec27e536130169b5d5f957e18e2b
Pete <p-morley@hotmail.co.uk>
2016-04-17 16:36:49 +0100

    Removed redundant code.

--- game.js
+++ game.js
@@ -126,21 +126,9 @@ function movePlayers() {
     p.lastMovedTime = Date.now();
     p.xGrid = Math.floor(p.x / gridSize) + numNegXGrids;
     p.yGrid = Math.floor(p.y / gridSize) + numNegYGrids;
-    playerIndexGrid[p.xGrid][p.yGrid].push(i);
+    //playerIndexGrid[p.xGrid][p.yGrid].push(i);
   }
 }
-
-/*function updateGrid() {
-  // Calculate players position in the grid.
-  p.xGrid = Math.floor(p.x / gridSize) + numNegXGrids;
-  p.yGrid = Math.floor(p.y / gridSize) + numNegYGrids;
-  if (p.xGrid != p.lastXGrid || p.yGrid != p.lastYGrid) {
-    // If this has changed, need to recalculate the set of objects sent to the player.
-    if (p.lastXGrid != null && p.lastYGrid != null && playerIndexGrid[p.lastXGrid] && playerIndexGrid[p.lastXGrid][p.lastYGrid])
-  }
-  p.lastXGrid = p.xGrid;
-  p.lastYGrid = p.yGrid;
-}*/
 
 function setupObjectGrids(objects) {
   var grids = [];
@@ -200,118 +188,8 @@ function getObjectsForPlayer(p, playerIndexGrid, objectIndexGrid) {
   };
 }
 
-function checkForCollosions(p, objects) {
-  // Check for collisions between a player and a set of objects.
-  // Removes the velocity component perpendicular to the line the player collides with.
-  var socket = io.sockets.connected[p.id];
-  var pCorners = [
-    [p.x - p.w / 2, p.y - p.h / 2],
-    [p.x + p.w / 2, p.y - p.h / 2],
-    [p.x - p.w / 2, p.y + p.h / 2],
-    [p.x + p.w / 2, p.y + p.h / 2]
-  ];
-
-  // Rotate 
-  for (var i = 0; i < 4; i++) {
-    pCorners[i] = [
-      p.x + (pCorners[i][0] - p.x) * Math.cos(p.angle) - (pCorners[i][1] - p.y) * Math.sin(p.angle),
-      p.y + (pCorners[i][0] - p.x) * Math.sin(p.angle) + (pCorners[i][1] - p.y) * Math.cos(p.angle)
-    ];
-  }
-
-  var pLines = [
-    [pCorners[0], pCorners[1]],
-    [pCorners[1], pCorners[2]],
-    [pCorners[2], pCorners[3]],
-    [pCorners[3], pCorners[0]]
-  ];
-
-  for (var i = 0; i < objects.length; i++) {
-    var o = objects[i];
-    var lines = []
-    var oCorners = [
-      [p.x - p.w / 2, p.y - p.h / 2],
-      [p.x + p.w / 2, p.y - p.h / 2],
-      [p.x - p.w / 2, p.y + p.h / 2],
-      [p.x + p.w / 2, p.y + p.h / 2]
-    ];
-
-    // Rotate 
-    for (var i = 0; i < 4; i++) {
-      oCorners[i] = [
-        p.x + (oCorners[i][0] - p.x) * Math.cos(0) - (oCorners[i][1] - p.y) * Math.sin(0),
-        p.y + (oCorners[i][0] - p.x) * Math.sin(0) + (oCorners[i][1] - p.y) * Math.cos(0)
-      ];
-    }
-    
-    var oLines = [
-      [oCorners[0],oCorners[1]],
-      [oCorners[1],oCorners[2]],
-      [oCorners[2],oCorners[3]],
-      [oCorners[3],oCorners[0]]
-    ];
-
-    for (var j = 0; j < pLines.length; j++) {
-      for (var k = 0; k < oLines.length; k++) {
-        var intersection = lineIntersection(
-          pLines[j][0][0],
-          pLines[j][0][1],
-          pLines[j][1][0],
-          pLines[j][1][1],
-          oLines[k][0][0],
-          oLines[k][0][1],
-          oLines[k][1][0],
-          oLines[k][1][1]
-        )
-
-        if (intersection.onLine1 && intersection.onLine2) {
-          // Only keep the velocity in the direction of the line the player hit.
-          var magnitude = Math.sqrt(Math.pow(p.vX,2) + Math.pow(p.vY,2));
-          p.vX = magnitude * Math.cos(Math.atan2(oLines[k][0][1] - oLines[k][1][1], oLines[k][0][0] - oLines[k][1][0]))
-          p.vY = magnitude * Math.sin(Math.atan2(oLines[k][0][1] - oLines[k][1][1], oLines[k][0][0] - oLines[k][1][0]))
-        }
-      }
-    }
-  }
-}
-
-function lineIntersection(l1StartX, l1StartY, l1EndX, l1EndY, l2StartX, l2StartY, l2EndX, l2EndY) {
-  // if the lines intersect, the result contains the x and y of the intersection (treating the lines as infinite) and booleans for whether line segment 1 or line segment 2 contain the point
-  var denominator, a, b, numerator1, numerator2, result = {
-      x: null,
-      y: null,
-      onLine1: false,
-      onLine2: false
-  };
-  denominator = ((l2EndY - l2StartY) * (l1EndX - l1StartX)) - ((l2EndX - l2StartX) * (l1EndY - l1StartY));
-  if (denominator == 0) {
-      return result;
-  }
-  a = l1StartY - l2StartY;
-  b = l1StartX - l2StartX;
-  numerator1 = ((l2EndX - l2StartX) * a) - ((l2EndY - l2StartY) * b);
-  numerator2 = ((l1EndX - l1StartX) * a) - ((l1EndY - l1StartY) * b);
-  a = numerator1 / denominator;
-  b = numerator2 / denominator;
-
-  // if we cast these lines infinitely in both directions, they intersect here:
-  result.x = l1StartX + (a * (l1EndX - l1StartX));
-  result.y = l1StartY + (a * (l1EndY - l1StartY));
-
-  // if line1 is a segment and line2 is infinite, they intersect if:
-  if (a > 0 && a < 1) {
-      result.onLine1 = true;
-  }
-  // if line2 is a segment and line1 is infinite, they intersect if:
-  if (b > 0 && b < 1) {
-      result.onLine2 = true;
-  }
-  // if line1 and line2 are segments, they intersect if both of the above are true
-  return result;
-}
-
 function sendUpdates() {
-  var playerIndexGrid = sortObjectsIntoGrids(players);
+  //var playerIndexGrid = sortObjectsIntoGrids(players);
 
   for (var i = 0; i < players.length; i++) {
     var p = players[i];
@@ -324,9 +202,6 @@ function sendUpdates() {
       "objects": objects,
       "scores": scores
     }
-  
-    // Check for collisions between player and local objects.
-    //checkForCollosions(p, objsToSend.objects);
 
     // Get socket of player.
     var socket = io.sockets.connected[p.id];
