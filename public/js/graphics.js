433d39f7a082e51d62e08a57c211627e1909c6ae
Philip Bruland <phil_7445@hotmail.co.uk>
2016-04-17 09:46:47 +0100

    Menial Graphics updates, no big feature.

--- public/js/graphics.js
+++ public/js/graphics.js
@@ -7,11 +7,11 @@ var CAR_GLASS_COLOR="#6699ff"
 
 var backgroundcanvas;
 
-function drawbuilding(ctx, color, pos_x, pos_y, width, height)
+function drawbuilding(ctx, pos_x, pos_y, width, height)
 {
   console.log("OH HEY");
   //BODY
-  ctx.fillStyle = color;
+  ctx.fillStyle = #101010;
   ctx.fillRect(pos_x, pos_y, width, height);
 }
 
@@ -46,25 +46,33 @@ function initbackground()
   backgroundcanvas = document.createElement("canvas");
 }
 
-function drawbackground(buildingarray, rotation, canvas)
+function getLocalCoords(player, canvas, x, y)
+{
+// Return coordinates for the user's canvas based when
+// given global coordinates.
+  return {
+           x:canvas.width/2 - (player.x-x),
+           y:canvas.height/2 - (player.y-y)
+         };
+}
+
+function drawObjects(objects, player, canvas)
 {
   var ctx = canvas.getContext("2d");*/
   ctx.save();
   ctx.translate(canvas.width/2, canvas.height/2);
   ctx.rotate(rotation*Math.PI/180);
   var i;
-  for(i = 0; i < buildingarray.length; i++)
+  for(i = 0; i < objects.length; i++)
   {
+    getLocalCoords(player, canvas, x, y);
     drawbuilding(ctx,
-                 buildingarray[i].color,
-                 buildingarray[i].pos_x,
-                 buildingarray[i].pos_y,
-                 buildingarray[i].width,
-                 buildingarray[i].height);
-    console.log("color " + buildingarray.color);
+                 objects[i].pos_x,
+                 objects[i].pos_y,
+                 objects[i].width,
+                 objects[i].height);
   }
   ctx.restore();
-  console.log("rotation " + rotation);
 }
 
 function building(color, pos_x, pos_y, width, height)
@@ -76,7 +84,7 @@ function building(color, pos_x, pos_y, width, height)
   this.height = height;
 }
 
-/*var buildingarray = [
+/*var objects = [
   new building("#00FF00", 50, 50, 100, 100),
   new building("#00FF00", 100, 100, 100, 100)
 ]*/
@@ -84,5 +92,5 @@ function building(color, pos_x, pos_y, width, height)
 function draw(canvas)
 {
   drawcar(12,12);
-  drawbackground(buildingarray, 5, canvas);
+  drawbackground(objects, 5, canvas);
 }
